9e7bbbc89404d6d1bc04c0f69418c28ab48c72c8
Daniel Jorge <djpgonzaga@gmail.com>
2022-12-08 00:37:32 -0300

    feat: code block title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <head />
-      <body className="pl-safe-left pt-safe-top pb-safe-bottom pr-safe-right">
+      <body className="pl-safe-left pt-safe-top pb-safe-bottom pr-safe-right antialiased">
         {children}
       </body>
     </html>
   );
 }
+// @TODO add custom select color
+// antialiased selection:bg-purple-500/90 selection:text-white
