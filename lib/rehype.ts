14413fb4a2bb26dbf111e39ae7c1f407928cf248
Daniel Jorge <djpgonzaga@gmail.com>
2022-12-11 23:41:53 -0300

    feat: word highlighted

--- lib/rehype.ts
+++ lib/rehype.ts
@@ -17,8 +17,18 @@ const autolinkHeadingsOptions: AutolinkHeadingsOptions = {
 
 const prettyCodeOptions: Partial<PrettyCodeOptions> = {
   theme: "poimandres",
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and
+    // allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{ type: "text", value: " " }];
+    }
+  },
   onVisitHighlightedLine(node) {
     node.properties.className.push("line--highlighted");
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ["word--highlighted"];
   },
 };
 
@@ -34,3 +44,6 @@ export const rehype = {
     slug: slugOptions,
   },
 };
+
+// import poimandres from 'shiki/themes/poimandres.json'
+// poimandres.
