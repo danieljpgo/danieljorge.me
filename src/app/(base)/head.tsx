5f513e2062ca80d11313e562bb6616edd27101cd
Daniel Jorge <djpgonzaga@gmail.com>
2022-12-18 17:15:19 -0300

    feat: add social meta tags

--- src/app/(base)/head.tsx
+++ src/app/(base)/head.tsx
@@ -7,12 +7,38 @@ export default function Head() {
     <>
       <title>Daniel Jorge</title>
       <meta name="description" content="My corner on the web" />
-      <head>
-        <meta
-          property="og:image"
-          content={`${url}/api/og?title=teste og image`}
-        />
-      </head>
+      <meta
+        property="og:image"
+        content={`${url}/api/og?title=teste og image`}
+      />
+
+      {/* <!-- Facebook Meta Tags --> */}
+      <meta property="og:url" content={`${url}`} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="teste og image" />
+      <meta
+        property="og:description"
+        content="An open source application built using the new router, server components and everything new in Next.js 13.es "
+      />
+      <meta
+        property="og:image"
+        content={`${url}/api/og?title=teste og image`}
+      />
+
+      {/* <!-- Twitter Meta Tags --/> */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content={url} />
+      <meta property="twitter:url" content={`${url}`} />
+      <meta name="twitter:title" content="teste og image" />
+      <meta
+        name="twitter:description"
+        content="An open source application built using the new router, server components and everything new in Next.js 13.es "
+      />
+      <meta
+        name="twitter:image"
+        content={`${url}/api/og?title=teste og image`}
+      />
+
       <HeadTags />
     </>
   );
