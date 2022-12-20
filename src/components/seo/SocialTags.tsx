dcd34b7115155c8cffde64a6c5aa3919f2e92356
Daniel Jorge <djpgonzaga@gmail.com>
2022-12-20 01:16:10 -0300

    fix: testing itemProp att

--- src/components/seo/SocialTags.tsx
+++ src/components/seo/SocialTags.tsx
@@ -17,50 +17,52 @@ export default function SocialTags(props: SocialTags) {
 
   console.log(process.env.NODE_ENV === "development");
 
-  return (
-    <>
-      <title>Daniel Jorge</title>
-      <meta name="description" content="Frontend Engineer at Bitso" />
-      <meta property="og:url" content={url} />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="Daniel Jorge" />
-      <meta property="og:description" content="Frontend Engineer at Bitso" />
-      <meta property="og:image" content={image} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta
-        property="twitter:domain"
-        content="danieljorge-gwk1i1uka-danieljpgo.vercel.app"
-      />
-      <meta property="twitter:url" content={url} />
-      <meta name="twitter:title" content="Daniel Jorge" />
-      <meta name="twitter:description" content="Frontend Engineer at Bitso" />
-      <meta name="twitter:image" content={image} />
-    </>
-  );
-
   // return (
   //   <>
-  //     <meta name="og:title" content={title} />
-  //     <meta name="og:description" content={description} />
-  //     <meta name="og:image" content={image} />
-  //     <meta property="og:image:width" content="1600" />
-  //     <meta property="og:image:height" content="830" />
-  //     <meta property="og:site_name" content="Daniel Jorge" />
-  //     {/* <!-- Facebook Meta Tags --> */}
-  //     <meta property="og:type" content={type} />
-  //     <meta property="og:title" content={title} />
-  //     <meta property="og:description" content={description} />
+  //     <title>Daniel Jorge</title>
+  //     <meta name="description" content="Frontend Engineer at Bitso" />
   //     <meta property="og:url" content={url} />
+  //     <meta property="og:type" content="website" />
+  //     <meta property="og:title" content="Daniel Jorge" />
+  //     <meta property="og:description" content="Frontend Engineer at Bitso" />
   //     <meta property="og:image" content={image} />
-  //     <meta property="og:image:alt" content="Daniel Jorge" />
-  //     {/* <!-- Twitter Meta Tags --> */}
   //     <meta name="twitter:card" content="summary_large_image" />
-  //     <meta property="twitter:domain" content={domain} />
+  //     <meta
+  //       property="twitter:domain"
+  //       content="danieljorge-gwk1i1uka-danieljpgo.vercel.app"
+  //     />
   //     <meta property="twitter:url" content={url} />
-  //     <meta name="twitter:title" content={title} />
-  //     <meta name="twitter:description" content={description} />
+  //     <meta name="twitter:title" content="Daniel Jorge" />
+  //     <meta name="twitter:description" content="Frontend Engineer at Bitso" />
   //     <meta name="twitter:image" content={image} />
-  //     <meta name="twitter:creator" content="@danieljpgo" />
   //   </>
   // );
+
+  return (
+    <>
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:image" content={image} />
+      <meta property="og:image:width" content="1600" />
+      <meta property="og:image:height" content="830" />
+      <meta property="og:site_name" content="Daniel Jorge" />
+      {/* <!-- Facebook Meta Tags --> */}
+      <meta property="og:type" content={type} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" itemProp="image" content={image} />
+      <meta property="og:image:alt" content="Daniel Jorge" />
+      {/* <!-- Twitter Meta Tags --> */}
+      {/* <meta name="twitter:card" content="summary_large_image" /> */}
+      <meta property="twitter:domain" content={domain} />
+      <meta property="twitter:url" content={url} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <meta name="twitter:creator" content="@danieljpgo" />
+      <meta name="twitter:site" content="@danieljpgo" />
+    </>
+  );
 }
