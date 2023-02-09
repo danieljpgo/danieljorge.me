cb4067fa53f2ee1c08214314028f45597653f116
Daniel Jorge <djpgonzaga@gmail.com>
2023-02-09 02:30:40 -0300

    feat: improve markup

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -15,75 +15,77 @@ export default function Home() {
   const notesPublished = notes.filter((note) => note.status === "published");
 
   return (
-    <section className="grid gap-8 md:px-10">
-      <div className="flex gap-4">
-        <Image
-          quality={100}
-          src={profile}
-          height={72}
-          width={72}
-          placeholder="blur"
-          alt="Daniel Jorge profile picture"
-          className="rounded-full"
-        />
-        <div className="flex flex-col gap-2 self-center">
-          <h1 className="text-lg font-medium leading-4 text-gray-800 antialiased">
-            Daniel Jorge
-          </h1>
-          <p className="text-base font-medium leading-4 text-slate-500 antialiased">
-            Frontend Engineer
-          </p>
+    <>
+      <section className="grid gap-8 md:px-10">
+        <div className="flex gap-4">
+          <Image
+            quality={100}
+            src={profile}
+            height={72}
+            width={72}
+            placeholder="blur"
+            alt="Daniel Jorge profile picture"
+            className="rounded-full"
+          />
+          <div className="flex flex-col gap-2 self-center">
+            <h1 className="text-lg font-medium leading-4 text-gray-800 antialiased">
+              Daniel Jorge
+            </h1>
+            <p className="text-base font-medium leading-4 text-slate-500 antialiased">
+              Frontend Engineer
+            </p>
+          </div>
         </div>
-      </div>
-      <Text>
-        Engineer interested at Functional Programming, React, Design System and
-        TypeScript, currently working at{" "}
-        <a
-          href="https://bitso.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="font-normal text-gray-800 underline decoration-gray-800 underline-offset-2 transition-colors duration-200 hover:text-gray-500 hover:decoration-gray-500 active:text-gray-400 active:decoration-gray-400"
-        >
-          Bitso
-        </a>
-        .
-      </Text>
-      <ul aria-label="Social links" className="flex gap-3">
-        <li>
+        <Text>
+          Engineer interested at Functional Programming, React, Design System
+          and TypeScript, currently working at{" "}
           <a
-            aria-label="Github"
-            href="https://github.com/danieljpgo"
+            href="https://bitso.com/"
             target="_blank"
             rel="noreferrer"
-            className="text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
+            className="font-normal text-gray-800 underline decoration-gray-800 underline-offset-2 transition-colors duration-200 hover:text-gray-500 hover:decoration-gray-500 active:text-gray-400 active:decoration-gray-400"
           >
-            <GithubIcon />
+            Bitso
           </a>
-        </li>
-        <li>
-          <a
-            aria-label="Twitter"
-            href="https://twitter.com/danieljpgo"
-            target="_blank"
-            rel="noreferrer"
-            className="text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
-          >
-            <TwitterIcon />
-          </a>
-        </li>
-        <li>
-          <a
-            aria-label="Linkedin"
-            href="https://www.linkedin.com/in/danieljpgo/"
-            target="_blank"
-            rel="noreferrer"
-            className="text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
-          >
-            <LinkedinIcon />
-          </a>
-        </li>
-      </ul>
-      <div className="grid gap-4">
+          .
+        </Text>
+        <ul aria-label="Social links" className="flex gap-3">
+          <li>
+            <a
+              aria-label="Github"
+              href="https://github.com/danieljpgo"
+              target="_blank"
+              rel="noreferrer"
+              className="text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
+            >
+              <GithubIcon />
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="Twitter"
+              href="https://twitter.com/danieljpgo"
+              target="_blank"
+              rel="noreferrer"
+              className="text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
+            >
+              <TwitterIcon />
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="Linkedin"
+              href="https://www.linkedin.com/in/danieljpgo/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
+            >
+              <LinkedinIcon />
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section className="grid gap-4 md:px-10">
         <div className="grid gap-2">
           <Heading
             as="h2"
@@ -129,7 +131,7 @@ export default function Home() {
             </li>
           ))}
         </ul>
-      </div>
+      </section>
       {/* <div className="grid gap-4">
         <div className="grid gap-2">
           <Heading
@@ -152,6 +154,6 @@ export default function Home() {
           </li>
         </ul>
       </div> */}
-    </section>
+    </>
   );
 }
