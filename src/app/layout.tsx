6673013095f5f8c4a27654ab691ae82f74f2aa15
Daniel Jorge <djpgonzaga@gmail.com>
2023-04-25 23:54:42 -0300

    feat: first draft for new metada data api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import "~/styles/globals.css";
 
@@ -16,3 +17,48 @@ export default function RootLayout({ children }: RootLayoutProps) {
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  title: {
+    default: "Daniel Jorge",
+    template: "%s | Daniel Jorge",
+  },
+  description: "Developer, writer.",
+  openGraph: {
+    title: "Daniel Jorge",
+    description: "Developer, writer.",
+    url: "http://danieljorge.me/",
+    siteName: "Daniel Jorge",
+    // images: [
+    //   {
+    //     url: "http://danieljorge.me/og.jpg",
+    //     width: 1920,
+    //     height: 1080,
+    //   },
+    // ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Daniel Jorge",
+    card: "summary_large_image",
+  },
+  icons: {
+    shortcut: "/favicon.ico",
+  },
+  // verification: {
+  //   google: "",
+  //   yandex: "",
+  // },
+};
