1f070d36c152a75fc0f803e40ecc69e61960ce4b
Daniel Jorge <djpgonzaga@gmail.com>
2023-06-13 23:13:53 -0300

    fix: remove draft notes and add /diagrams routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,20 +2,22 @@ import type { MetadataRoute } from "next";
 import { notes, diagrams } from "~/lib/contentlayer";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const notesRoute = notes.map((note) => ({
-    url: `https://danieljorge.me/notes/${note.slug}`,
-    lastModified: note.publishedAt.split("T")[0],
-  }));
+  const notesRoute = notes
+    .filter((note) => note.status === "published")
+    .map((note) => ({
+      url: `https://danieljorge.me/notes/${note.slug}`,
+      lastModified: note.publishedAt.split("T")[0],
+    }));
 
   const diagramsRoute = diagrams.map((diagram) => ({
     url: `https://danieljorge.me/diagrams/${diagram.slug}`,
     lastModified: diagram.createdAt.split("T")[0],
   }));
 
-  const routes = [""].map((route) => ({
+  const routes = ["", "/diagrams"].map((route) => ({
     url: `https://danieljorge.me${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...notesRoute, ...diagramsRoute];
+  return [...routes, ...diagramsRoute, ...notesRoute];
 }
