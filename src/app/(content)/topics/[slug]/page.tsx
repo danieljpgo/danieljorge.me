36a9436610ddba6da58f74c6ecea1da930654111
Daniel Jorge <djpgonzaga@gmail.com>
2023-06-30 22:40:27 -0300

    fix: remove layout file

--- src/app/(content)/topics/[slug]/page.tsx
+++ src/app/(content)/topics/[slug]/page.tsx
@@ -6,6 +6,7 @@ import { documents } from "~/lib/contentlayer";
 import { Heading, Text, View } from "~/components";
 import { formatDateNumerical } from "~/lib/date";
 import { genericMetadata } from "~/lib/metadata";
+import { cn } from "~/lib/tailwindcss";
 
 type TopicProps = {
   params: { slug: keyof typeof topics };
@@ -22,70 +23,99 @@ export default function Topic({ params }: TopicProps) {
 
   return (
     <>
-      <div className="flex flex-col gap-2 pt-7">
-        <Heading
-          as="h1"
-          size="2xl"
-          weight="semibold"
-          leading="tight"
-          color="darker"
-        >
-          {topics[params.slug]}
-        </Heading>
-        <Text color="base">{`Writings, notes, and more related to ${
-          topics[params.slug]
-        }`}</Text>
-      </div>
-      <hr />
-      <div className="grid gap-4">
-        <ul className="grid gap-4">
-          {selectedDocuments.map((content) => (
-            <li key={content.slug}>
-              <article className="grid gap-1">
-                <Link href={content._raw.flattenedPath}>
-                  <Heading
-                    as="h3"
-                    size="base"
-                    weight="medium"
-                    leading="tight"
-                    color="darker"
-                  >
-                    {content.title}
-                  </Heading>
-                </Link>
-                <div className="flex gap-2">
-                  <Text size="sm" color="light">
-                    {(() => {
-                      if ("createdAt" in content) {
-                        return content.createdAtFormatted;
-                      }
-                      return content.publishedAtFormatted;
-                    })()}
-                  </Text>
-                  <Text size="sm" color="light">
-                    •
-                  </Text>
-                  <Text size="sm" color="light">
-                    {/* @ts-expect-error: */}
-                    <View slug={content.slug} type="view" /> views
-                  </Text>
-                </div>
-              </article>
-            </li>
-          ))}
-        </ul>
-        <hr />
-        <div className="flex justify-center pb-8">
-          <Link
-            href="/"
-            className="group flex gap-2 text-sm text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
+      <aside
+        className={cn(
+          "sticky top-8 hidden h-min w-full max-w-[14rem] justify-start gap-2.5 lg:grid xl:max-w-[16rem]",
+          "mt-[100px]",
+        )}
+      >
+        <nav className="grid gap-1">
+          <hr className="my-1.5" />
+          <a
+            href="#"
+            className="text-sm text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
           >
-            <span className="translate-x-0 transition-transform duration-200 group-hover:translate-x-[2px] group-active:translate-x-[-2px]">
-              ←
-            </span>
-            Home
-          </Link>
+            Back to top
+          </a>
+        </nav>
+      </aside>
+      <article className="grid w-full max-w-2xl gap-4">
+        <div className="flex flex-col gap-2 pt-7">
+          <Heading
+            as="h1"
+            size="2xl"
+            weight="semibold"
+            leading="tight"
+            color="darker"
+          >
+            {topics[params.slug]}
+          </Heading>
+          <Text color="base">{`Writings, notes, and more related to ${
+            topics[params.slug]
+          }`}</Text>
         </div>
+        <hr />
+        <div className="grid gap-4">
+          <ul className="grid gap-4">
+            {selectedDocuments.map((content) => (
+              <li key={content.slug}>
+                <article className="grid gap-1">
+                  <Link href={content._raw.flattenedPath}>
+                    <Heading
+                      as="h3"
+                      size="base"
+                      weight="medium"
+                      leading="tight"
+                      color="darker"
+                    >
+                      {content.title}
+                    </Heading>
+                  </Link>
+                  <div className="flex gap-2">
+                    <Text size="sm" color="light">
+                      {(() => {
+                        if ("createdAt" in content) {
+                          return content.createdAtFormatted;
+                        }
+                        return content.publishedAtFormatted;
+                      })()}
+                    </Text>
+                    <Text size="sm" color="light">
+                      •
+                    </Text>
+                    <Text size="sm" color="light">
+                      {/* @ts-expect-error: */}
+                      <View slug={content.slug} type="view" /> views
+                    </Text>
+                  </div>
+                </article>
+              </li>
+            ))}
+          </ul>
+          <hr />
+          <div className="flex justify-center pb-8">
+            <Link
+              href="/"
+              className="group flex gap-2 text-sm text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
+            >
+              <span className="translate-x-0 transition-transform duration-200 group-hover:translate-x-[2px] group-active:translate-x-[-2px]">
+                ←
+              </span>
+              Home
+            </Link>
+          </div>
+        </div>
+      </article>
+      <div className="hidden h-min w-full max-w-[14rem] justify-end pt-8 xl:flex xl:max-w-[16rem]">
+        <Link
+          href="/"
+          className="group flex gap-2 text-sm text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
+        >
+          <span className="translate-x-0 transition-transform duration-200 group-hover:translate-x-[2px] group-active:translate-x-[-2px]">
+            ←
+          </span>
+          Home
+        </Link>
       </div>
     </>
   );
