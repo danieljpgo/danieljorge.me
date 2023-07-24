6eb1a71312cadca6675091a0e181e2e638c6f50a
Daniel Jorge <djpgonzaga@gmail.com>
2023-07-23 23:58:23 -0300

    feat: new urls for notes

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/css-a-set-of-boundaries-not-hard-rules",
+        destination: "/css-boundaries",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
