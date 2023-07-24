a9a00a2d5cb3fc4168466ae6d011a9df9d0fcae3
Daniel Jorge <djpgonzaga@gmail.com>
2023-07-24 10:29:26 -0300

    feat: new urls for diagrams

--- next.config.js
+++ next.config.js
@@ -9,8 +9,28 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/css-a-set-of-boundaries-not-hard-rules",
-        destination: "/css-boundaries",
+        source: "/notes/css-a-set-of-boundaries-not-hard-rules",
+        destination: "/notes/css-boundaries",
+        permanent: true,
+      },
+      {
+        source: "/diagrams/begin-with-low-level-components",
+        destination: "/diagrams/begin-low",
+        permanent: true,
+      },
+      {
+        source: "/diagrams/co-locate-your-providers-and-stores",
+        destination: "/diagrams/colocate-providers",
+        permanent: true,
+      },
+      {
+        source: "/diagrams/create-components-through-composition",
+        destination: "/diagrams/composition",
+        permanent: true,
+      },
+      {
+        source: "/diagrams/url-as-a-way-to-compose-your-interface",
+        destination: "/diagrams/url-compose",
         permanent: true,
       },
     ];
