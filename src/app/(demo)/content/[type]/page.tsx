01c54488574d2ceb5c97b3d1393dac42cc852a6c
Daniel Jorge <djpgonzaga@gmail.com>
2023-07-31 00:57:06 -0300

    feat: dynamic page for all content lists

--- src/app/(demo)/content/[type]/page.tsx
+++ src/app/(demo)/content/[type]/page.tsx
@@ -1,22 +1,115 @@
 import type { Metadata } from "next";
 import type { DocumentTypeNames } from "contentlayer/generated";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { formatDateNumerical } from "~/lib/date";
 import { documents, routes } from "~/lib/contentlayer";
 import { genericMetadata } from "~/lib/metadata";
-// import Link from "next/link";
-// import { cn } from "~/lib/tailwindcss";
-// import { configs } from "~/lib/contentlayer";
-// import { genericMetadata } from "~/lib/metadata";
-// import { Heading, Text, Mdx, View } from "~/components";
-// import { topics } from "~/lib/content";
+import { cn } from "~/lib/tailwindcss";
+import { Heading, Text, View } from "~/components";
 
 type ContentProps = {
   params: { type: string };
 };
+export default function Content({ params }: ContentProps) {
+  const contents = documents.filter(
+    (doc) => doc._raw.sourceFileDir === params.type,
+  );
 
-export default function Content(props: ContentProps) {
-  return <div>JSX</div>;
+  return (
+    <>
+      <aside
+        className={cn(
+          "sticky top-8 hidden h-min w-full max-w-[14rem] justify-start gap-2.5 lg:grid xl:max-w-[16rem]",
+          "mt-[100px]",
+        )}
+      >
+        <nav className="grid gap-1">
+          <hr className="my-1.5" />
+          <a
+            href="#"
+            className="text-sm text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
+          >
+            Back to top
+          </a>
+        </nav>
+      </aside>
+      <article className="grid w-full max-w-2xl gap-4">
+        <div className="flex flex-col gap-2 pt-7">
+          <Heading
+            as="h1"
+            size="2xl"
+            weight="semibold"
+            leading="tight"
+            color="darker"
+          >
+            {/* Configs */}
+            {metadatas[contents[0].type].title}
+          </Heading>
+          <Text color="base">{metadatas[contents[0].type].description}</Text>
+        </div>
+        <hr />
+        <div className="grid gap-4">
+          <ul className="grid gap-4">
+            {contents.map((content) => (
+              <li key={content.slug}>
+                <article className="grid gap-1">
+                  <Link href={content._raw.flattenedPath}>
+                    <Heading
+                      as="h2"
+                      size="base"
+                      weight="medium"
+                      leading="tight"
+                      color="darker"
+                    >
+                      {content.title}
+                    </Heading>
+                  </Link>
+                  <div className="flex gap-2">
+                    <Text size="sm" color="light">
+                      {"createdAtFormatted" in content
+                        ? content.createdAtFormatted
+                        : content.publishedAtFormatted}
+                    </Text>
+                    <Text size="sm" color="light">
+                      •
+                    </Text>
+                    <Text size="sm" color="light">
+                      {/* @ts-expect-error: */}
+                      <View slug={content.slug} type="view" /> views
+                    </Text>
+                  </div>
+                </article>
+              </li>
+            ))}
+          </ul>
+          <hr />
+          <div className="flex justify-center pb-8">
+            <Link
+              href="/"
+              className="group flex gap-2 text-sm text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
+            >
+              <span className="translate-x-0 transition-transform duration-200 group-hover:translate-x-[2px] group-active:translate-x-[-2px]">
+                ←
+              </span>
+              Home
+            </Link>
+          </div>
+        </div>
+      </article>
+      <div className="hidden h-min w-full max-w-[14rem] justify-end pt-8 xl:flex xl:max-w-[16rem]">
+        <Link
+          href="/"
+          className="group flex gap-2 text-sm text-gray-700 transition-colors duration-200 hover:text-gray-400 active:text-gray-300"
+        >
+          <span className="translate-x-0 transition-transform duration-200 group-hover:translate-x-[2px] group-active:translate-x-[-2px]">
+            ←
+          </span>
+          Home
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export function generateStaticParams(): Array<ContentProps["params"]> {
