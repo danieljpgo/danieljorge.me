ed291d944818261d3c52b1608fc91319c8ee6b79
Daniel Jorge <djpgonzaga@gmail.com>
2024-01-23 23:56:42 -0300

    feat: unifying messages

--- lib/content.ts
+++ lib/content.ts
@@ -1,63 +1,76 @@
 import type { DocumentTypeNames } from "contentlayer/generated";
 
-export const topics = {
-  web: "Web",
-  css: "CSS",
-  react: "React",
-  ui: "UI",
-  ["design-system"]: "Design System",
-  productivity: "Productivity",
-} as const;
-
-export const CATEGORY = {
+export const OG = {
   HOME: "home",
+  INDEX: "index",
   CONTENT: "content",
   CONTENT_IMAGE: "content-image",
   CONTENT_AUTO_IMAGES: "content-auto-images",
-  LIST: "list",
 } as const;
 
-export const documentTypeMap = {
-  Diagrams: "diagrams",
-  Notes: "notes",
-  Writings: "writings",
-  Configs: "configs",
-  Crafts: "crafts",
-} as const satisfies Record<DocumentTypeNames, string>;
+export const DOCUMENT_TYPE = {
+  NOTES: "notes",
+  CRAFTS: "crafts",
+  CONFIGS: "configs",
+  DIAGRAMS: "diagrams",
+  WRITINGS: "writings",
+} as const;
 
-export const documentCategoryMap = {
-  Diagrams: "content-auto-images",
-  Notes: "content",
-  Writings: "content",
-  Configs: "content",
-  Crafts: "content-image",
-} as const satisfies Record<
-  DocumentTypeNames,
-  (typeof CATEGORY)[keyof typeof CATEGORY]
->;
+export const TOPICS = {
+  UI: "ui",
+  CSS: "css",
+  WEB: "web",
+  REACT: "react",
+  PRODUCTIVITY: "productivity",
+  DESIGN_SYSTEM: "desigs-system",
+} as const;
 
 export const messages = {
-  diagrams: {
-    title: "Diagrams",
-    description: "Explanations, concepts, design solutions, created over time.",
-  },
-  notes: {
+  [TOPICS.UI]: "UI",
+  [TOPICS.CSS]: "CSS",
+  [TOPICS.WEB]: "Web",
+  [TOPICS.REACT]: "React",
+  [TOPICS.PRODUCTIVITY]: "Productivity",
+  [TOPICS.DESIGN_SYSTEM]: "Design System",
+  [DOCUMENT_TYPE.NOTES]: {
     title: "Notes",
     description: "Loose, short-form thoughts, reflections, and ideas.",
   },
-  writings: {
-    title: "Writings",
-    description: "Crafted, long form thoughts and ideas.",
-  },
-  configs: {
-    title: "Configs",
-    description: "Settings, shortcuts and everything related to productivity.",
-  },
-  crafts: {
+  [DOCUMENT_TYPE.CRAFTS]: {
     title: "Crafts",
     description: "Build, concepts, techniques and solutions",
   },
-} as const satisfies Record<
-  (typeof documentTypeMap)[keyof typeof documentTypeMap],
-  { title: keyof typeof documentTypeMap; description: string }
->;
+  [DOCUMENT_TYPE.CONFIGS]: {
+    title: "Configs",
+    description: "Settings, shortcuts and everything related to productivity.",
+  },
+  [DOCUMENT_TYPE.DIAGRAMS]: {
+    title: "Diagrams",
+    description: "Explanations, concepts, design solutions, created over time.",
+  },
+  [DOCUMENT_TYPE.WRITINGS]: {
+    title: "Writings",
+    description: "Crafted, long form thoughts and ideas.",
+  },
+} as const;
+
+//@TODO more type safety?
+
+export const documentTypeMap: Record<DocumentTypeNames, string> = {
+  Notes: DOCUMENT_TYPE.NOTES,
+  Crafts: DOCUMENT_TYPE.CRAFTS,
+  Configs: DOCUMENT_TYPE.CONFIGS,
+  Diagrams: DOCUMENT_TYPE.DIAGRAMS,
+  Writings: DOCUMENT_TYPE.WRITINGS,
+} as const;
+
+export const documentOGMap: Record<
+  DocumentTypeNames,
+  (typeof OG)[keyof typeof OG]
+> = {
+  Notes: OG.CONTENT,
+  Crafts: OG.CONTENT_IMAGE,
+  Configs: OG.CONTENT,
+  Diagrams: OG.CONTENT_AUTO_IMAGES,
+  Writings: OG.CONTENT,
+} as const;
